580d311151f15a1d98b5330f636e60b0aa53556b
Vollbier1589 <83216654+Vollbier1589@users.noreply.github.com>
2021-11-18 16:27:49 +0100

    copypaste

--- js/opsys.js
+++ js/opsys.js
@@ -44,3 +44,122 @@ if ('localStorage' in window || 'sessionStorage' in window) {
 
   window.addEventListener('storage', onStorageChanged);
 }
+
+
+function getReadFile(reader, i) {
+  return function () {
+    var li = document.querySelector('[data-idx="' + i + '"]');
+
+    li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
+  }
+}
+
+function readFiles(files) {
+  document.getElementById('count').innerHTML = files.length;
+
+  var target = document.getElementById('target');
+  target.innerHTML = '';
+
+  for (var i = 0; i < files.length; ++i) {
+    var item = document.createElement('li');
+    item.setAttribute('data-idx', i);
+    var file = files[i];
+
+    var reader = new FileReader();
+    reader.addEventListener('load', getReadFile(reader, i));
+    reader.readAsText(file);
+
+    item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
+    target.appendChild(item);
+  };
+}
+
+async function writeFile() {
+  if (!window.chooseFileSystemEntries) {
+    alert('Native File System API not supported');
+    return;
+  }
+
+  const target = document.getElementById('target');
+  target.innerHTML = 'Opening file handle...';
+
+  const handle = await window.chooseFileSystemEntries({
+    type: 'save-file',
+  });
+
+  const file = await handle.getFile()
+  const writer = await handle.createWriter();
+  await writer.write(0, 'Hello world from What Web Can Do!');
+  await writer.close()
+
+  target.innerHTML = 'Test content written to ' + file.name + '.';
+}
+
+
+function readContacts() {
+  var api = (navigator.contacts || navigator.mozContacts);
+
+  if (api && !!api.select) { // new Chrome API
+    api.select(['name', 'email'], {multiple: true})
+      .then(function (contacts) {
+        consoleLog('Found ' + contacts.length + ' contacts.');
+        if (contacts.length) {
+          consoleLog('First contact: ' + contacts[0].name + ' (' + contacts[0].email + ')');
+        }
+      })
+      .catch(function (err) {
+        consoleLog('Fetching contacts failed: ' + err.name);
+      });
+
+  } else if (api && !!api.find) { // old Firefox OS API
+    var criteria = {
+      sortBy: 'familyName',
+      sortOrder: 'ascending'
+    };
+
+    api.find(criteria)
+      .then(function (contacts) {
+        consoleLog('Found ' + contacts.length + ' contacts.');
+        if (contacts.length) {
+          consoleLog('First contact: ' + contacts[0].givenName[0] + ' ' + contacts[0].familyName[0]);
+        }
+      })
+      .catch(function (err) {
+        consoleLog('Fetching contacts failed: ' + err.name);
+      });
+
+  } else {
+    consoleLog('Contacts API not supported.');
+  }
+}
+
+function consoleLog(data) {
+  var logElement = document.getElementById('log');
+  logElement.innerHTML += data + '\n';
+}
+
+
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
+    });
+}
+
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
+
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  }
+}
