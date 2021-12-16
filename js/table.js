899be9db592164362cf91a540a955f7a47c990d5
Edgar <edgar.meilinger@stud.fra-uas.de>
2021-12-16 14:44:35 +0100

    A

---
+++ js/table.js
@@ -0,0 +1,43 @@
+// Creating and adding data to first row of the table
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Sr. No.";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Name";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "Company";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+thead.appendChild(row_1);
+
+
+// Creating and adding data to second row of the table
+let row_2 = document.createElement('tr');
+let row_2_data_1 = document.createElement('td');
+row_2_data_1.innerHTML = "1.";
+let row_2_data_2 = document.createElement('td');
+row_2_data_2.innerHTML = "James Clerk";
+let row_2_data_3 = document.createElement('td');
+row_2_data_3.innerHTML = "Netflix";
+
+row_2.appendChild(row_2_data_1);
+row_2.appendChild(row_2_data_2);
+row_2.appendChild(row_2_data_3);
+tbody.appendChild(row_2);
+
+
+// Creating and adding data to third row of the table
+let row_3 = document.createElement('tr');
+let row_3_data_1 = document.createElement('td');
+row_3_data_1.innerHTML = "2.";
+let row_3_data_2 = document.createElement('td');
+row_3_data_2.innerHTML = "Adam White";
+let row_3_data_3 = document.createElement('td');
+row_3_data_3.innerHTML = "Microsoft";
+
+row_3.appendChild(row_3_data_1);
+row_3.appendChild(row_3_data_2);
+row_3.appendChild(row_3_data_3);
+tbody.appendChild(row_3);
